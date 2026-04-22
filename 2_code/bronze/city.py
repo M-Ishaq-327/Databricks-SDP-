9b5679712b004baf5606dcc111bb0a1cbfd2e3f8
M.ISHAQ <engineerishaq.py@gmail.com>
2026-04-22 11:11:43 +0500

    define func to read data in BRONZE layer

--- 2_code/bronze/city.py
+++ 2_code/bronze/city.py
@@ -4,3 +4,11 @@ from pyspark.sql.functions import md5, concat_ws, sha2
 
 # Configuration
 SOURCE_PATH = "s3://goodcabs-mi//city"
+
+
+def city_bronze():
+    df = spark.read.format("csv").option("header", "true").option("inferSchema", "true").option("mode", "PERMISSIVE").option("mergeSchema", "true").option("columnNameOfCorruptRecord","_corrupt_record").load(SOURCE_PATH)
+
+    df = df.withColumn("file_name", col("_metadata.file_path")).withColumn("ingest_datetime", current_timestamp())
+    
+    return df
