87f932c63c15e96449551465d0b8c4d07a378962
M.ISHAQ <engineerishaq.py@gmail.com>
2026-04-22 11:15:31 +0500

    set the materialized view

--- 2_code/silver/city.py
+++ 2_code/silver/city.py
@@ -1,7 +1,17 @@
 from pyspark import pipelines as dp
 from pyspark.sql import functions as F
 
-
+@dp.materialized_view(
+    name="transportation.silver.city",
+    comment="Cleaned and standardized products dimension with business transformations",
+    table_properties={
+        "quality": "silver",
+        "layer": "silver",
+        "delta.enableChangeDataFeed": "true",
+        "delta.autoOptimize.optimizeWrite": "true",
+        "delta.autoOptimize.autoCompact": "true"
+    }
+)
 
 def city_silver():
     df_bronze = spark.read.table("transportation.bronze.city")
