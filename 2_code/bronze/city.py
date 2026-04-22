95a2544127f4086f12a9dbedbe4f560d17a68c34
M.ISHAQ <engineerishaq.py@gmail.com>
2026-04-22 11:10:37 +0500

    set the s3 path to fetech data for pipeline

--- 2_code/bronze/city.py
+++ 2_code/bronze/city.py
@@ -1,3 +1,6 @@
 from pyspark import pipelines as dp
 from pyspark.sql.functions import col, current_timestamp
 from pyspark.sql.functions import md5, concat_ws, sha2
+
+# Configuration
+SOURCE_PATH = "s3://goodcabs-mi//city"
