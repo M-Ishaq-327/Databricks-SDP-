201c705693a05f6832fc545a1589ce1d04e76b8e
M.ISHAQ <engineerishaq.py@gmail.com>
2026-04-22 11:18:39 +0500

    define func for transofrming trips data from bronze to silver layer

--- 2_code/bronze/trips.py
+++ 2_code/bronze/trips.py
@@ -2,3 +2,24 @@ from pyspark import pipelines as dp
 import pyspark.sql.functions as F
 
 SOURCE_PATH = "s3://goodcabs-mi/trips"
+
+
+def orders_bronze():
+    df = (
+        spark.readStream.format("cloudFiles")
+        .option("cloudFiles.format", "csv")
+        .option("cloudFiles.inferColumnTypes", "true")
+        .option("cloudFiles.schemaEvolutionMode", "rescue")
+        .option("cloudFiles.maxFilesPerTrigger", 100)
+        .load(SOURCE_PATH)
+    )
+
+     # Rename the problematic column
+    df = df.withColumnRenamed(
+        "distance_travelled(km)",
+        "distance_travelled_km"
+    )
+
+    df = df.withColumn("file_name", F.col("_metadata.file_path")).withColumn("ingest_datetime", F.current_timestamp())
+
+    return df
